fc517bc2fb39b61ccaafd2cbc428f09ca73fe953
2637309949@qq.com <2637309949@qq.com>
2025-04-11 17:48:20 +0800

    fix(apigate-service): update API routes and file upload aliases
    - Update file upload aliases to use 'fileupload' action instead of 'file'
    - Improve code formatting and readability of route configurations

--- apigate-service/index.js
+++ apigate-service/index.js
@@ -14,35 +14,37 @@ createWeb(
     withName(pkg.name),
     withSettings({
         port,
-        routes: [{
-            path: '/api',
-            cors: true,
-            autoAliases: true,
-            authorization: true,
-            onBeforeCall(ctx, route, req, res) {
-                ctx.meta.headers = req.headers
-                ctx.meta.host = req.headers['host']
-                ctx.meta.authorization = req.headers['authorization']
-                ctx.meta.userAgent = req.headers['user-agent']
+        routes: [
+            {
+                path: '/api',
+                cors: true,
+                autoAliases: true,
+                authorization: true,
+                onBeforeCall(ctx, route, req, res) {
+                    ctx.meta.headers = req.headers
+                    ctx.meta.host = req.headers['host']
+                    ctx.meta.authorization = req.headers['authorization']
+                    ctx.meta.userAgent = req.headers['user-agent']
+                },
             },
-        },
-        {
-            path: '/upload',
-            authorization: false,
-            cors: true,
-            aliases: {
-                'POST /multipart': 'multipart:file.multipart',
-                'POST /stream': 'stream:file.stream',
-            }
-        },
-        {
-            path: '',
-            authorization: false,
-            aliases: {
-                'GET /login': 'apigate.login',
-                'GET /register': 'apigate.register',
-            }
-        }],
+            {
+                path: '/upload',
+                authorization: false,
+                cors: true,
+                aliases: {
+                    'POST /multipart': 'multipart:fileupload.multipart',
+                    'POST /stream': 'stream:fileupload.stream',
+                }
+            },
+            {
+                path: '',
+                authorization: false,
+                aliases: {
+                    'GET /login': 'apigate.login',
+                    'GET /register': 'apigate.register',
+                }
+            },
+        ],
         assets: {
             folder: './public',
             options: { index: ['index.html', 'index.htm'] }
